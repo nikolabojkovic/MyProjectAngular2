a2372b276647f507fa8c12146eb9e2f52bba89f3
Nikola <nikolabojkovic@gmail.com>
2017-07-30 01:46:22 +0200

    Portfolio page added, css files and custom js files added

---
+++ src/app/portfolio/portfolio.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'portfolio',
+    templateUrl: 'portfolio.component.html',
+    styleUrls: [ 
+        'portfolio.component.css',
+        '../shared/css/main.css',
+        '../shared/css/responsive.css',
+        '../shared/css/site.css'
+     ]
+})
+export class PortfolioComponent { }
